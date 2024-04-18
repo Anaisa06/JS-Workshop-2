7b2f13b62f96a82df9214ce098bd3a3f78c80aa0
anaisa06 <isa.alzate98@gmail.com>
2024-04-18 15:53:12 -0500

    Complete challenge two - section one

--- Sección 1/reto2.js
+++ Sección 1/reto2.js
@@ -5,21 +5,23 @@ let listaSouvenirs = []
 
 //Función para verificar si el valor ingresado es un número o no
 function verificarNumero (variable){
-    while(isNaN(parseFloat(variable))){
+    while(isNaN(variable)){
         console.warn("El valor ingresado no es un número")
         variable = prompt("Por favor, ingresa solo números")
-    }    
+    }
+    variable = parseFloat(variable)
+    return variable   
 }
 
 
 //Función para verificar la disponibilidad
 function verificarDisponibilidad (variable){
-    while (variable != "1" && variable !="2"){
-           
-        console.warn("No es una respuesta válida")
-        variable = prompt ("No es una respuesta válida, intenta de nuevo")            
-        
+    if (variable === true){
+        variable = "Disponible"
+    } else {
+        variable = "No disponible"
     }
+    return variable             
 }
 
 //Pregunta al usuario si quiere añadir un nuevo souvenir
@@ -31,35 +33,26 @@ while(nuevoSouvenir === "1"){
 
     //Pregunta el nombre del artículo
     let nombreSouv = prompt("Ingresa el nombre de souvenir")
-    console.log(nombreSouv)    
+    console.log(nombreSouv + " " + typeof(nombreSouv))    
 
     //Pregunta el precio del artículo
-    let precioSouv = prompt("Ingresa el precio del souvenir")    
-    console.log(precioSouv)
-    //Utiliza la función para verificar si el valor ingresado es un número
-    verificarNumero(precioSouv)
-
-    //Pregunta si el artículo está disponible o no
-    let disponibilidadSouv = prompt("¿El souvenir está disponible en el momento? 1. Si, 2. No")
-    //Utiliza la función para verificar que sea un dato válido
-    verificarDisponibilidad(disponibilidadSouv)
-
-
-    /* Falta conversión a true o false 
-    if (disponibilidadSouv === "1"){
-        disponibilidadSouv = "Si"            
-        console.log("El souvenir está disponible")
-    } else if (disponibilidadSouv === "2"){
-        disponibilidadSouv = "No"
-        console.log("El souvenir no está disponible")      
-    }
-    */
-
+    let precioSouv = prompt("Ingresa el precio del souvenir")     
+    //Utiliza la función para verificar si el valor ingresado es un número, convertir a número y asignar a nueva variable
+    let precioSouvNum = verificarNumero(precioSouv)
+    console.log(precioSouvNum + " " + typeof(precioSouvNum))
     
 
-    let souvenir = {
-        "nombre del souvenir": nombreSouv, 
-        "precio" : precioSouv, 
+    //Pregunta si el artículo está disponible o no
+    let disponibilidadSouvBool = confirm("¿El souvenir está disponible en el momento?")
+    console.log(typeof(disponibilidadSouvBool))
+    //Utiliza la función para verificar la disponibilidad y asignar a una nueva variable
+    let disponibilidadSouv = verificarDisponibilidad(disponibilidadSouvBool)
+    console.log(disponibilidadSouv)  
+      
+
+    const souvenir = {
+        "nombre": nombreSouv, 
+        "precio" : `$${precioSouvNum}`, 
         "disponibilidad" : disponibilidadSouv
     }
 
@@ -71,4 +64,12 @@ while(nuevoSouvenir === "1"){
 
 console.log(listaSouvenirs)
 
-const verSouv = prompt("¿Quieres ver la lista de souvenirs?")
+const verSouv = confirm("¿Quieres ver los souvenirs que registraste?")
+if (verSouv){
+    listaSouvenirs.forEach(souvenir => {
+        alert( `- ${souvenir.nombre} - ${souvenir.precio} - ${souvenir.disponibilidad}`)
+    }
+        )
+} 
+
+alert("Hasta pronto!")
